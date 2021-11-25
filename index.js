06ad2fcc83a724f0aa77bd6185bc62b04c1a9dde
Yoseph <yosephten@gmail.com>
2021-11-25 15:34:33 +0300

    Responsive Design added

--- index.js
+++ index.js
@@ -1,6 +1,76 @@
-import menu from './menu.mjs';
+var menu = {    
+    breakfast:[
+        {
+            name:"Breakfast 1",
+            price:350,
+            img:"assets/placeholder.png"
+        },
+        {
+            name:"Breakfast 2",
+            price:200,
+            img:"assets/placeholder.png"
+        },
+        {
+            name:"Breakfast 3",
+            price:100,
+            img:"assets/about.jpeg"
+        }
+    ],
+    lunchDinner:[
+        {
+            name:"Lunch 1",
+            price:350,
+            img:"assets/placeholder.png"
+        },
+        {
+            name:"Dinner 2",
+            price:200,
+            img:"assets/placeholder.png"
+        },
+        {
+            name:"Lunch 3",
+            price:100,
+            img:"assets/placeholder.png"
+        }
+    ],
+    juice:[
+        {
+            name:"Juice 1",
+            price:350,
+            img:"assets/placeholder.png"
+        },
+        {
+            name:"Juice 2",
+            price:200,
+            img:"assets/placeholder.png"
+        },
+        {
+            name:"Juice 3",
+            price:100,
+            img:"assets/placeholder.png"
+        }
+    ],
+    hotDrinks:[
+        {
+            name:"Hot Drink 1",
+            price:35,
+            img:"assets/placeholder.png"
+        },
+        {
+            name:"Hot Drink 2",
+            price:20,
+            img:"assets/placeholder.png"
+        },
+        {
+            name:"Hot Drink 3",
+            price:10,
+            img:"assets/placeholder.png"
+        }
+    ]
+        
+}
 
-window.onload = populate();
+populate();
 
 $(window).scroll(function () {
     $(".intro-pic").css("background-position","50% " + ($(this).scrollTop() / 2.5) + "px");
